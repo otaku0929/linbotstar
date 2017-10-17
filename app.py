ba44bb6bb9afcd9c8435747f7a38231eb5158c5c
Victor <32451715+otaku0929@users.noreply.github.com>
2017-10-18 00:37:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -427,7 +427,7 @@ def handle_message(event):
         return 0
     if event.message.text == "抽鮮肉":
         client = ImgurClient('33ed33e765afedc', '04f0d5531b1d0978ff97fd990554c899e9e7e1f5')
-        images = client.get_album_images('boys')
+        images = client.get_album_images('9eQni')
         index = random.randint(0, len(images) - 1)
         url = images[index].link
         image_message = ImageSendMessage(
