7ac5c4bc57639d37410248229c9f9c76f991658e
Victor <32451715+otaku0929@users.noreply.github.com>
2017-10-18 09:52:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -555,7 +555,7 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, buttons_template)
         return 0
-     if event.message.text == "影音":
+    if event.message.text == "影音":
         buttons_template = TemplateSendMessage(
             alt_text='影音 template',
             template=ButtonsTemplate(
