a9b48fdb64c76d37aa8629791f04af3d7d3b506f
Victor <32451715+otaku0929@users.noreply.github.com>
2017-10-19 00:21:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -468,6 +468,25 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token, image_message)
         return 0
+    if event.message.text == "抽抽":
+        
+        client = ImgurClient('33ed33e765afedc', '04f0d5531b1d0978ff97fd990554c899e9e7e1f5')
+        imagesb = client.get_album_images('9eQni')
+        index = random.randint(0, len(imagesb) - 1)
+        urlb = images[index].link
+        
+        imageg = requests.get(API_Get_Image)
+        urlg = imageg.json().get('Url')
+        
+        url = random.choice(urlb,urlg)
+        
+        image_message = ImageSendMessage(
+            original_content_url=url,
+            preview_image_url=url
+        )
+        line_bot_api.reply_message(
+            event.reply_token, image_message)
+        return 0
     if event.message.text == "PTT廢文":
         content = ptt_hot()
         line_bot_api.reply_message(
