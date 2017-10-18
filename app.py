2f11ab2e2ab213a219887c47386baa7681555266
Victor <32451715+otaku0929@users.noreply.github.com>
2017-10-18 09:25:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -514,8 +514,8 @@ def handle_message(event):
                         text='新聞'
                     ),
                     MessageTemplateAction(
-                        label='電影',
-                        text='電影'
+                        label='電影音樂',
+                        text='電影音樂'
                     ),
                     MessageTemplateAction(
                         label='PTT看版',
@@ -555,7 +555,7 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, buttons_template)
         return 0
-    if event.message.text == "電影":
+    if event.message.text == "電影音樂":
         buttons_template = TemplateSendMessage(
             alt_text='電影音樂 template',
             template=ButtonsTemplate(
@@ -564,7 +564,7 @@ def handle_message(event):
                 thumbnail_image_url='https://i.imgur.com/sbOTJt4.png',
                 actions=[
                     MessageTemplateAction(
-                        label='上映中電影精選',
+                        label='院線電影精選',
                         text='看電影'
                     ),
                     MessageTemplateAction(
