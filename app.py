f864ffcb23f4610d97fe7c7974c4b533a287f248
Victor <32451715+otaku0929@users.noreply.github.com>
2017-10-20 23:31:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -576,7 +576,7 @@ def handle_message(event):
             TextSendMessage(text=content))
         return 0
     if event.message.text == "抽籤":
-        content = aska()
+        content = ask()
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=content))
