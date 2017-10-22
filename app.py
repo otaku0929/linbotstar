6970aa7c83e5ebf098d549cf9dfc0bbfedd721e9
Victor <32451715+otaku0929@users.noreply.github.com>
2017-10-22 21:43:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -882,7 +882,7 @@ def handle_message(event):
         return 0
     
     if len(mlist) ==2:
-        if mlist in [ "美金","港幣","英鎊","澳幣","加拿大幣","新加坡幣","瑞士法郎","日圓","南非幣","瑞典幣","紐元","泰幣","菲國比索","印尼幣","歐元","韓元","南盾","馬來幣","人民幣"]:
+        if mlist in [ "美金","港幣","英鎊","澳幣","加拿大幣","新加坡幣","瑞士法郎","日圓","南非幣","瑞典幣","紐元","泰幣","菲國比索","印尼幣","歐元","韓元","越南盾","馬來幣","人民幣"]:
             res = mlist
             content = rate(res)
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text=content))
