ee3319c711872cf23daeafc0c1dd737f9bf5ac2e
Victor <32451715+otaku0929@users.noreply.github.com>
2017-11-05 21:16:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -936,10 +936,15 @@ def handle_message(event):
         index = random.randint(0, len(images) - 1)
         urlb = images[index].link
         
+        client = ImgurClient('33ed33e765afedc', '04f0d5531b1d0978ff97fd990554c899e9e7e1f5')
+        images = client.get_album_images('23p2B')
+        index = random.randint(0, len(images) - 1)
+        urlbv = images[index].link
+        
         imageg = requests.get(API_Get_Image)
         urlg = imageg.json().get('Url')
-        
-        url = random.choice([urlg,urlb,urlg,urlb,urlg,urlg,urlg])
+
+        url = random.choice([urlg,urlb,urlg,urlb,urlg,urlg,urlg,urlgv])
         
         image_message = ImageSendMessage(
             original_content_url=url,
