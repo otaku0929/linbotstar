81224ec256340a0997469fb6eb7d4f51340624ac
Victor <32451715+otaku0929@users.noreply.github.com>
2017-11-12 21:45:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,19 +30,6 @@ client_secret = config['imgur_api']['Client_Secret']
 album_id = config['imgur_api']['Album_ID']
 API_Get_Image = config['other_api']['API_Get_Image']
 
-# OAuth credential placeholders that must be filled in by users.
-# You can find them on
-# https://www.yelp.com/developers/v3/manage_app
-YELPCLIENT_ID = '9VbMjEdGSCCfUHBkiqLRHA'
-YELPCLIENT_SECRET = 'xxxxxxxxx'
-
-# API constants, you shouldn't have to change these.
-API_HOST = 'https://api.yelp.com'
-SEARCH_PATH = '/v3/businesses/search'
-BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
-TOKEN_PATH = '/oauth2/token'
-GRANT_TYPE = 'client_credentials'
-
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -967,68 +954,6 @@ def fwords(resf):
                 content = talk_messages(messages_talk)
                 return content
             
-def obtain_bearer_token(host, path):
-    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
-    assert CLIENT_ID, "Please supply your client_id."
-    assert CLIENT_SECRET, "Please supply your client_secret."
-    data = urlencode({
-        'client_id': YELPCLIENT_ID,
-        'client_secret': YELPCLIENT_SECRET,
-        'grant_type': GRANT_TYPE,
-    })
-    headers = {
-        'content-type': 'application/x-www-form-urlencoded',
-    }
-    response = requests.request('POST', url, data=data, headers=headers)
-    bearer_token = response.json()['access_token']
-    return bearer_token
-
-def request(host, path, bearer_token, url_params=None):
-    url_params = url_params or {}
-    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
-    headers = {
-        'Authorization': 'Bearer %s' % bearer_token,
-    }
-
-    response = requests.request('GET', url, headers=headers, params=url_params)
-    return response.json()
-
-
-def request(host, path, bearer_token, url_params=None):
-    url_params = url_params or {}
-    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
-    headers = {
-        'Authorization': 'Bearer %s' % bearer_token,
-    }
-
-    response = requests.request('GET', url, headers=headers, params=url_params)
-    return response.json()
-
-
-def search(bearer_token, term, location):
-    url_params = {
-        'term': term.replace(' ', '+'),
-        'location': location.replace(' ', '+'),
-        'limit': SEARCH_LIMIT,
-        'radius': 1000
-    }
-    return request(API_HOST, SEARCH_PATH, bearer_token, url_params=url_params)
-
-def get_restaurant(term, location):
-    bearer_token = obtain_bearer_token(API_HOST, TOKEN_PATH)
-    response = search(bearer_token, term, location)
-    businesses = response.get('businesses')
-    
-    restaurants = []
-    for business in businesses:
-        restaurant = {}
-        restaurant['name'] = business['name']
-        restaurant['address'] = business['location']['display_address'][0]
-        restaurant['photo'] = business['image_url']
-        restaurant['yelp_url'] = business['url']
-        restaurants.append(restaurant)
-    return restaurants
-
 def talk_messages(messages_talk):
 
     if messages_talk in {'幹','操'}:
