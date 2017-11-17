5377d29ddabffab4bf540a85fb44fcfbfea2190b
Victor <32451715+otaku0929@users.noreply.github.com>
2017-11-17 21:41:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1036,7 +1036,7 @@ def handle_message(event):
     mlist = event.message.text
     words = event.message.text
 #    profile = line_bot_api.get_profile(event.source.user_id)
-    #gprofile = line_bot_api.get_room_member_profile(room_id, user_id)
+    gprofile = line_bot_api.get_profile(event.source.group_id)
 
     if event.message.text == "eyny":
         content = eyny_movie()
