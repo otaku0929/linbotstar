c105d447ea4590fa550cef54b091e9fc500989c7
Victor <32451715+otaku0929@users.noreply.github.com>
2017-11-22 11:07:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1209,7 +1209,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token, image_message)
         return 0
-    if event.message.text == "抽":
+    if event.message.text in ["抽","來點正能量"]:
         
         client = ImgurClient('33ed33e765afedc', '04f0d5531b1d0978ff97fd990554c899e9e7e1f5')
         images = client.get_album_images('9eQni')
