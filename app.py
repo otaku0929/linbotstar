83316e67a3e1ed8ec327f6e44142a84e8b95823a
Victor <32451715+otaku0929@users.noreply.github.com>
2018-06-22 12:52:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1216,6 +1216,7 @@ def yelp(location):
               }
 
     resp = requests.get(url=url, params=params, headers=headers)
+    print(headers)
     print(resp.json())
     businesses = resp.json()['businesses']
 
