e3dfaa89d5002ad71b4aa2a5850f5dc008d04173
Victor <32451715+otaku0929@users.noreply.github.com>
2018-06-25 14:39:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2424,7 +2424,7 @@ def handle_message(event):
             )
             line_bot_api.reply_message(
                 event.reply_token, [image_message, TextSendMessage(text=content)])
-    return 0
+        return 0
     #匯率
     rate_list = "美金|港幣|英鎊|澳幣|加拿大幣|新加坡幣|瑞士法郎|日圓|日幣|南非幣|瑞典幣|紐元|泰幣|菲國比索|印尼幣|歐元|韓元|越南盾|馬來幣|人民幣"
     if re.search(rate_list,event.message.text):
