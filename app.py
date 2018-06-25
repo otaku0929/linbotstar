3129c62187f07aad7823bfdee37386315d2babfd
Victor <32451715+otaku0929@users.noreply.github.com>
2018-06-25 13:56:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2295,8 +2295,7 @@ def handle_message(event):
     #查塔羅牌說明
     if re.search("查(塔羅牌|塔羅|tarot)說明(\d+)",event.message.text):
         tarot_content = tarot_detail(re.search("查(塔羅牌|塔羅|tarot)說明(\d+)",event.message.text).group(2))
-        print(tarot_content)
-        content = '{}\n\n{}'.format(tarot_content[0][1],tarot_content[0][2])
+        content = '{}\n\n{}\{}'.format(tarot_content[1],tarot_content[2],tarot_content[3])
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=content))
