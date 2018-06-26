9ce8e8da5c97cbdbca7dd9a917d9432831e27f54
Victor <32451715+otaku0929@users.noreply.github.com>
2018-06-26 16:30:45 +0800

    Add files via upload

---
+++ function/rate.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import pandas
+import datetime
+
+def rate(res):
+
+    url = "http://rate.bot.com.tw/xrt?Lang=zh-TW"
+    dfs = pandas.read_html(url)
+    currency = dfs[0]
+    currency = currency.ix[:,0:3]
+    currency.columns = [u'貨幣',u'匯率(賣)',u'匯率(買)']
+    currency[u'貨幣'] = currency[u'貨幣'].str.split('\(',1).str[0]
+    currency[u'貨幣'] = currency[u'貨幣'].str.split().str[0]
+
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    for a in currency.index:
+        data = currency.ix[a,0]
+        if data == res:
+            title = currency.ix[a,0]
+            rate =currency.ix[a,2]
+            ratedata = '{} 1:{}'.format(title,rate)   
+
+    return '臺灣銀行牌告匯率\n查詢時間{}\n{}'.format(now,ratedata)
+
+
+if __name__ == '__main__':
+    res = '美金'
+    print(rate(res))
