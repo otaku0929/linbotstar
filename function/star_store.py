f0ad573d26e75425d1f9912b2115be046b5a30cf
Victor <32451715+otaku0929@users.noreply.github.com>
2018-06-30 22:21:26 +0800

    Update star_store.py

--- function/star_store.py
+++ function/star_store.py
@@ -11,19 +11,19 @@ def star_store():
     dict=[
             {
                 'title':'夜光卡通造型汽車臨時停車卡',
-                'url':'http://seller.pcstore.com.tw/S198614447/C1200056412.htm',
+                'url':'https://seller.pcstore.com.tw/S198614447/C1200056412.htm',
                 'image':'https://i.imgur.com/jrg2cmw.jpg',
                 'detail':'環保硅膠材質，柔軟可見，不易變形'
                     },
             {
                 'title':'長效防蚊扣',
-                'url':'http://seller.pcstore.com.tw/S198614447/C1200281208.htm',
+                'url':'https://seller.pcstore.com.tw/S198614447/C1200281208.htm',
                 'image':'https://i.imgur.com/90WjznL.jpg',
                 'detail':'使用時效長，拆封使用就能有效驅趕蚊蟲時效長達2-3個月～',
                     },
             {
                 'title':'匠師的故鄉美景農場',
-                'url':'http://0426815735.tw.tranews.com/',
+                'url':'https://0426815735.tw.tranews.com/',
                 'image':'https://i.imgur.com/ISbRAsA.jpg',
                 'detail':'匠師的故鄉‧包子匠師‧濱海體驗‧台中旅遊',
                     }
@@ -34,5 +34,6 @@ def star_store():
     return dict[0:]
 
 if __name__ == '__main__':
-    print(star_store())
+    store_content = star_store()
+    print(store_content[0]['title'])
 
