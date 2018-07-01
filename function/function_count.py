0479b5cb615e3e8453422ca850577707d7987b1e
Victor <32451715+otaku0929@users.noreply.github.com>
2018-07-01 20:53:39 +0800

    Update function_count.py

--- function/function_count.py
+++ function/function_count.py
@@ -21,7 +21,33 @@ def gs_write(res):
     n += 1
     worksheet.update_acell(res,n)
 
+def gs_read():
+    
+    GDriveJSON = 'star-lucky777-580f56621f40.json'
+    GSpreadSheet = 'lucky777'
+
+    scope = ['https://spreadsheets.google.com/feeds']
+    key = SAC.from_json_keyfile_name(GDriveJSON, scope)
+    gc = gspread.authorize(key)
+    worksheet = gc.open(GSpreadSheet).sheet1
+    
+    fun_json=[]
+    
+    for obj in range(2,25):
+        json_obj={worksheet.acell('A'+str(obj)).value:worksheet.acell('B'+str(obj)).value}
+        fun_json.append(json_obj)
+    
+    return fun_json
+
+def get_fun_count():
+    
+    obj_json = gs_read()
+    
+    for obj in obj_json:
+        print (obj)
+    
+
 if __name__ == '__main__':
-    res = 'B5'
-    gs_write(res)
-    print('OK')
+    #res = 'B5'
+    #gs_write(res)
+    print(get_fun_count())
