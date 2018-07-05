2be9a95c4b86f04364169ea6f3809ece73d2c802
Victor <32451715+otaku0929@users.noreply.github.com>
2018-07-05 15:46:21 +0800

    Update ifoodie.py

--- function/ifoodie.py
+++ function/ifoodie.py
@@ -9,6 +9,13 @@ import re
 import json
 import requests
 import random
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
 
 def ifoodie(city,res):
     
@@ -25,12 +32,80 @@ def ifoodie(city,res):
     random.shuffle(food_list)
     
     return food_list[0:5][0]['restaurant']['name']
-    #id blog-id
+    #id
     #description
     #thumb
     #['restaurant']['name']
     
-    
+
+def ifoodie_line(reply_token, city,res):
+
+        ifoodie_content = ifoodie(city,res)
+        carousel_template_message = TemplateSendMessage(
+            alt_text='美食特搜結果',
+            template=CarouselTemplate(
+                columns=[
+                    CarouselColumn(
+                        thumbnail_image_url=ifoodie_content[0]['thumb'],
+                        title=ifoodie_content[0]['restaurant']['name'],
+                        text=ifoodie_content[0]['description'],
+                        actions=[
+                            URITemplateAction(
+                                label='詳細內容',
+                                uri= 'https://ifoodie.tw/blog/'+ifoodie_content[0]['id']
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url=ifoodie_content[1]['thumb'],
+                        title=ifoodie_content[1]['restaurant']['name'],
+                        text=ifoodie_content[1]['description'],
+                        actions=[
+                            URITemplateAction(
+                                label='詳細內容',
+                                uri= 'https://ifoodie.tw/blog/'+ifoodie_content[1]['id']
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url=ifoodie_content[2]['thumb'],
+                        title=ifoodie_content[2]['restaurant']['name'],
+                        text=ifoodie_content[2]['description'],
+                        actions=[
+                            URITemplateAction(
+                                label='詳細內容',
+                                uri= 'https://ifoodie.tw/blog/'+ifoodie_content[2]['id']
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url=ifoodie_content[3]['thumb'],
+                        title=ifoodie_content[3]['restaurant']['name'],
+                        text=ifoodie_content[3]['description'],
+                        actions=[
+                            URITemplateAction(
+                                label='詳細內容',
+                                uri= 'https://ifoodie.tw/blog/'+ifoodie_content[3]['id']
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url=ifoodie_content[4]['thumb'],
+                        title=ifoodie_content[4]['restaurant']['name'],
+                        text=ifoodie_content[4]['description'],
+                        actions=[
+                            URITemplateAction(
+                                label='詳細內容',
+                                uri= 'https://ifoodie.tw/blog/'+ifoodie_content[4]['id']
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(reply_token,carousel_template_message)
+        #gs_write('B27')
+        return 0
 
 if __name__ == '__main__':
     messages = '查美食=南京復興餐廳'
