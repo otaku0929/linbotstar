46a276d17976e76a907c867d29ab6074dcf6e246
Victor <32451715+otaku0929@users.noreply.github.com>
2018-07-09 01:50:11 +0800

    Update weatherparser.py

--- function/weatherparser.py
+++ function/weatherparser.py
@@ -89,10 +89,10 @@ class WeatherParser(object):
             H_24R = wp_json['records']['location'][0]['weatherElement'][7]['elementValue']
             #D_TX = wp_json['records']['location'][0]['weatherElement'][14]['elementValue']
             #D_TN = wp_json['records']['location'][0]['weatherElement'][16]['elementValue']
-            e = (HUMD/100)*6.105*math.exp(17.27*float(TEMP)/(237.7+float(TEMP)))      
+            e = float((HUMD/100)*6.105*math.exp(17.27*float(TEMP)/(237.7+float(TEMP))))      
             AT = round(1.07*float(TEMP)+0.2*e-0.65*float(WDSD)-2.7,1)
             
-            content = '觀測站：{}\n時間：{}\n溫度：{}°C\n體感溫度：{}°C\n濕度：{}%\n風速：{} m/s\n雨量：{} 毫米\n-----———————-\n資料來源：中央氣象局'.format(wp_loc,TIME[5:16],TEMP,AT,HUMD,WDSD,H_24R)
+            content = '觀測站：{}\n時間：{}\n溫度：{}°C\n體感溫度：{}°C\n濕度：{}%\n風速：{} m/s\n日累積雨量：{} 毫米\n-------------------\n資料來源：中央氣象局'.format(wp_loc,TIME[5:16],TEMP,AT,HUMD,WDSD,H_24R)
             return content
     
 
