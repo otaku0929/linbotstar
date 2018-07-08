8be21bdc78e61e0dfc43c3df2939952ca4d74766
Victor <32451715+otaku0929@users.noreply.github.com>
2018-07-08 23:43:53 +0800

    Update weatherparser.py

--- function/weatherparser.py
+++ function/weatherparser.py
@@ -11,30 +11,43 @@ import re
 from .get_wp_state import get_state
 
 import function.g_function
-gfunction = function.g_function.function()
+_function = function.g_function.function()
 
 def main():
-    messages = '天氣=台北公館'
+    wp = WeatherParser()
+    #print(wp.getReportWithAPI('龜山'))
+    messages = '查天氣=中壢高中'
     match = re.match('(.+)*天氣=(.+)',messages)
     loc = match.group(2)
     #print(loc)
-    wp = WeatherParser()
-    locA =  gfunction.getGeoForAddress(loc)
-    wp_name = get_state(locA,'C0')
+    #locA =  gfunction.getGeoForAddress(loc)
+    #wp_state = get_state(locA,'wp')
     #print(wp_name['state_name'])
     #print(wp_location)
-    wp_content = (wp.getReportWithAPI(wp_name['state_name']))
+    #print(wp_state)
+    #wp_content = (wp.getReportWithAPI(wp_state['state_name']))
+    wp_content = wp.getReportWithAPI(loc)
     content = '地點:{}\n{}'.format(loc,wp_content)
     print(content)
 
 class WeatherParser(object):
     
     def __init__(self):
-        self.doc_name = "O-A0001-001"#"F-C0032-001"  
+        self.doc_name_C0 = "O-A0001-001"#"F-C0032-001"
+        self.doc_name_46 = "O-A0003-001"
         self.user_key = "CWB-A01FD046-AA6B-4C27-A307-616C33DB89B7"
         #self.api_link = "http://opendata.cwb.gov.tw/opendataapi?dataid=%s&authorizationkey=%s" % (self.doc_name,self.user_key)
         
     def getReportWithAPI(self, location):
+        locA =  _function.getGeoForAddress(location)
+        wp_state = get_state(locA,'wp')
+        #print(wp_state)
+        wp_loc = wp_state['state_name']
+        
+        if re.match('^46(.+)',wp_state['state_no']):
+            doc_name = self.doc_name_46
+        elif re.match('^C0(.+)',wp_state['state_no']):
+            doc_name = self.doc_name_C0
                
        # https://opendata.cwb.gov.tw/api/v1/rest/datastore/
        #{dataid}? 資料編號
@@ -46,8 +59,8 @@ class WeatherParser(object):
         #print(location)
         headers = {'Authorization': self.user_key}
         #url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/%s' % self.doc_name
-        url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/%s?locationName=%s' % (self.doc_name, location)
-        #res = requests.get("http://opendata.cwb.gov.tw/api/v1/rest/datastore/%s?locationName=%s" % (self.doc_name, location), headers=headers)
+        url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/%s?locationName=%s' % (doc_name, wp_loc)
+        #res = requests.get("http://opendata.cwb.gov.tw/api/v1/rest/datastore/%s?locationName=%s" % (doc_name, wp_loc), headers=headers)
         res = requests.get(url, headers=headers)
         wp_json = res.json()
         
@@ -63,12 +76,15 @@ class WeatherParser(object):
         #WD15M
         #WS15T
         #print(wp_json['records']['location'][0]['weatherElement'])
+        if len(wp_json['records']['location']) == 0:
+            return '目前 {} 觀測站沒有任何資料'.format(wp_loc)
+        else:
         TIME = wp_json['records']['location'][0]['time']['obsTime']
         TEMP = wp_json['records']['location'][0]['weatherElement'][3]['elementValue']
         HUMD = wp_json['records']['location'][0]['weatherElement'][4]['elementValue']
         H_24R = wp_json['records']['location'][0]['weatherElement'][7]['elementValue']
         
-        content = '查詢時間:{}\n溫度:{} C\n濕度:{}%\n日累積雨量:{} 毫米'.format(TIME[0:16],TEMP,HUMD,H_24R)
+        content = '觀測站:{}\n查詢時間:{}\n溫度:{} C\n濕度:{}%\n日累積雨量:{} 毫米'.format(wp_loc,TIME[0:16],TEMP,HUMD,H_24R)
         return content
     
 
