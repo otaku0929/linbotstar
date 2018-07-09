fa764e40c84c9f0ee60aabba3dcf2d45fd272c97
Victor <32451715+otaku0929@users.noreply.github.com>
2018-07-09 13:52:09 +0800

    Update weatherparser.py

--- function/weatherparser.py
+++ function/weatherparser.py
@@ -77,8 +77,10 @@ class WeatherParser(object):
  
             if len(wp_json['records']['location']) > 0:
                 TIME = wp_json['records']['location'][0]['time']['obsTime']
+                _WDIR = float(wp_json['records']['location'][0]['weatherElement'][2]['elementValue'])
+                WDIR = _function.degToCompass(_WDIR)
                 WDSD = wp_json['records']['location'][0]['weatherElement'][2]['elementValue']
-                TEMP = wp_json['records']['location'][0]['weatherElement'][3]['elementValue']
+                TEMP = round(float(wp_json['records']['location'][0]['weatherElement'][3]['elementValue']),1)
                 HUMD = round(float(wp_json['records']['location'][0]['weatherElement'][4]['elementValue'])*100)
                 H_24R = wp_json['records']['location'][0]['weatherElement'][7]['elementValue']
                 #D_TX = wp_json['records']['location'][0]['weatherElement'][14]['elementValue']
@@ -87,8 +89,8 @@ class WeatherParser(object):
                 e = float(HUMD)/100*6.105*math.exp(17.27*float(TEMP)/(237.7+float(TEMP)))      
                 AT = round(1.07*float(TEMP)+0.2*e-0.65*float(WDSD)-2.7,1)
                 
-                content = '觀測站：{}\n時間：{}\n溫度：{}°C\n體感溫度：{}°C\n濕度：{}%\n風速：{} m/s\n日累積雨量：{} 毫米\n-------------------\n資料來源：中央氣象局'.format(wp_loc,TIME[5:16],TEMP,AT,HUMD,WDSD,H_24R)
-                return content 
+                content = '觀測站：{}\n時間：{}\n溫度：{}°C\n體感溫度：{}°C\n濕度：{}%\n風向：{}\n風速：{} m/s\n日累積雨量：{} 毫米\n-------------------\n資料來源：中央氣象局'.format(wp_loc,TIME[5:16],TEMP,AT,HUMD,WDIR,WDSD,H_24R)
+                return content
     
 
 if __name__ == '__main__':
