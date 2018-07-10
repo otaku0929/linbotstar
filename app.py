7851a23b71d1ae0ba90de0814b5d47aea98e3114
Victor <32451715+otaku0929@users.noreply.github.com>
2018-07-10 15:13:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1941,7 +1941,7 @@ def handle_message(event):
     #setting watermark
     if re.match('浮水印t=(.+)f=(\d+)ttf=(t.)p=(p.)',event.message.text):
         if event.source.type == 'user':
-            match =  re.match('浮水印t=(.+)f=(\d+)ttf=(t.)p=(p.)',messages)
+            match = re.match('浮水印t=(.+)f=(\d+)ttf=(t.)p=(p.)',event.message.text)
             #print (match.group(0))
             content = _function.set_watermark(event.source.userId,match.group(1),match.group(2),match.group(3),match.group(4))            
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text=content))
