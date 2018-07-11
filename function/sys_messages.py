89c1457294ec1aa33e2bb2d5c18846626a6b08f5
Victor <32451715+otaku0929@users.noreply.github.com>
2018-07-11 16:36:41 +0800

    Update sys_messages.py

--- function/sys_messages.py
+++ function/sys_messages.py
@@ -24,7 +24,7 @@ class sys_messages(object):
  *text:浮水印內容\n\
  *fn=字體大小\n\
  *ttf=字型：目前共有中文7,英文4種t1~t7 e1~e4 \n\
- *color=顏色:支援red|green|blue|white|break|pink|yellow|gold, 也可以輸入色票#ffffff\n\
+ *color=顏色:支援red|green|blue|white|black|pink|yellow|gold, 也可以輸入色票#ffffff\n\
  *aln=透明度:0~255 (建議不要小於128)\n\
  *pn=浮水印位置：以九宮格方式劃分'
     
