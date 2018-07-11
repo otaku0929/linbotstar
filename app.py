d7af3843ce0be7da01a4ceb7ab9fbfbf38a0a6b0
Victor <32451715+otaku0929@users.noreply.github.com>
2018-07-11 14:30:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1807,10 +1807,10 @@ def handle_message(event):
             TextSendMessage(text=content))
         return 0
     #setting watermark
-    if re.match('浮水印t=(.+)',markcontent):
+    if re.match('浮水印t=(.+)'event.message.text):
         if event.source.type == 'user':
             print (event.message.text)
-            match = re.search('t=(.+)f=(]d+)ttf=([t|e]\d)c=(red|green|blue|white|break|pink|yellow|gold|#......)',markcontent)
+            #match = re.search('t=(.+)f=(]d+)ttf=([t|e]\d)c=(red|green|blue|white|break|pink|yellow|gold|#......)',markcontent)
             #match = re.match('t=(.+)f=(\d+)ttf=([t|e]\d)c=(red|green|blue|white|break|pink|yellow|gold|#......)al=(\d+)p=(\d)',event.message.text)
             uid = event.source.user_id
             #print(uid)
