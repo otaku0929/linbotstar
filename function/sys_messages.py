c805c4ab33520e0688fcec3dd2b4a58a1f555ec2
Victor <32451715+otaku0929@users.noreply.github.com>
2018-07-11 15:40:30 +0800

    Update sys_messages.py

--- function/sys_messages.py
+++ function/sys_messages.py
@@ -16,15 +16,17 @@ class sys_messages(object):
         self.obj_name = 'sys_messages'
     
     def m_addmark(self):
-        return '請先設定浮水印輸出格式，方式如下:\n\
- 浮水印t=小星星浮水印f=52ttf=t4c=whiteal=128p=p9\n\
-  ------------\n\
-  *t=浮水印內容\n\
-  *f=字體大小\n\
-  *ttf=字型：目前共有中文7,英文4種t1~t7 e1~e4 \n\
-  *c=顏色:支援red|green|blue|white|break|pink|yellow|gold, 也可以輸入色票#ffffff\n\
-  *al=透明度:0~255 (建議不要小於128)\n\
-  *p=浮水印位置：以九宮格方式劃分'
+        return '此功能不能在群組使用\n\n\
+請先設定浮水印輸出格式，方式如下:\n\n\
+#浮水印%text%fn%ttf%color%al%pn\n\
+例如 #浮水印%小星星浮水印%f72%t3%red%aln255%p9\n\
+ ------------\n\
+ *text:浮水印內容\n\
+ *fn=字體大小\n\
+ *ttf=字型：目前共有中文7,英文4種t1~t7 e1~e4 \n\
+ *color=顏色:支援red|green|blue|white|break|pink|yellow|gold, 也可以輸入色票#ffffff\n\
+ *aln=透明度:0~255 (建議不要小於128)\n\
+ *pn=浮水印位置：以九宮格方式劃分'
     
     
 if __name__ == '__main__':
