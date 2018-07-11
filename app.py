7e001532db434440e3b9dcfbb5506b4cf52ce0bc
Victor <32451715+otaku0929@users.noreply.github.com>
2018-07-11 16:20:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1436,7 +1436,7 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text=content))
         return 0
     #查浮水印怎麼使用
-    if event.message.text in ['浮水印','浮水印功能','查浮水印','查浮水印怎麼用','浮水印怎麼使用','怎麼用浮水印']:
+    if event.message.text in ['浮水印','浮水印功能','查浮水印','查浮水印怎麼用','浮水印怎麼使用','怎麼用浮水印','查浮水印用法']:
         content = _sys_mg.m_addmark()
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=content))
         return 0        
