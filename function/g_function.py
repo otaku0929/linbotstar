44c0db4b6408b01dcd8871e16e2cbd44886e35cd
Victor <32451715+otaku0929@users.noreply.github.com>
2018-09-04 09:46:55 +0800

    Add files via upload

--- function/g_function.py
+++ function/g_function.py
@@ -23,12 +23,41 @@ def main():
     #print(get_hsing())
     gfunction = function()
     #print (gfunction.getDistance('121.7812','25.0712','21.4420','24.9976'))
-    print(gfunction.imgur_images_delete('AZL5bX9'))
+    #print(gfunction.imgur_images_delete('AZL5bX9'))
+    print (gfunction.weekDay(2018,9,1))
 
 class function(object):
     
     def __init__(self):
         self.name = 'start'
+        
+    def weekDay(self, year, month, day):
+        offset = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+        week   = ['Sunday', 
+                  'Monday', 
+                  'Tuesday', 
+                  'Wednesday', 
+                  'Thursday',  
+                  'Friday', 
+                  'Saturday']
+        afterFeb = 1
+        if month > 2: afterFeb = 0
+        aux = year - 1700 - afterFeb
+        # dayOfWeek for 1700/1/1 = 5, Friday
+        dayOfWeek  = 5
+        # partial sum of days betweem current date and 1700/1/1
+        dayOfWeek += (aux + afterFeb) * 365                  
+        # leap year correction    
+        dayOfWeek += aux / 4 - aux / 100 + (aux + 100) / 400     
+        # sum monthly and day offsets
+        dayOfWeek += offset[month - 1] + (day - 1)              
+        dayOfWeek %= 7
+        int_day = int(dayOfWeek) 
+
+        #weekdate_s = '%s-%s-%s'%(year,month,day-int_day+1)
+        #weekdate_e = '%s-%s-%s'%(year,month,day+6-int_day)
+        
+        return int_day, week[int_day] 
 
     def is_number(self,string):
         try:
--- function/life_zone.py
+++ function/life_zone.py
@@ -9,18 +9,93 @@ import requests
 import re
 import json
 from bs4 import BeautifulSoup
+import datetime, pytz
+
+import function.g_function
+_g_function = function.g_function.function()
 
 
 def main():
 
     _life=life_zone()
-    messages='天堂M黑妖練等'
-    return(_life.gamer(messages))
+    message='天秤座'
+    content = _life.starweek(message)
+    print(content)
+    #return(_life.gamer(messages))
     
 class life_zone(object):
     
     def __init__(self):
         self.class_name = 'life_zone'
+        
+    def starweek(self,res):
+        
+        time = str(datetime.datetime.now(pytz.timezone('Asia/Taipei')))[0:10]
+        year = int(time[0:4])
+        month = int(time[5:7])
+        date = int(time[8:10])
+
+        today_res = _g_function.weekDay(year, month, date)
+
+        
+        weekdate_s = str(datetime.datetime.now(pytz.timezone('Asia/Taipei'))-datetime.timedelta(days=int(today_res[0])))[0:10]
+        weekdate_e = str(datetime.datetime.now(pytz.timezone('Asia/Taipei'))+datetime.timedelta(days=(6-int(today_res[0]))))[0:10]             
+        
+        #print(weekdate_s,weekdate_e)
+
+        starnumber = self.starnumber(res)
+        
+        url = 'http://astro.click108.com.tw/weekly_10.php?iAcDay=%s&iType=1&iAstro=%s'%(time,starnumber)
+        
+        request = requests.get(url)
+        soup = BeautifulSoup(request.content, "html.parser")
+        soup_class = soup.select("div[class='TODAY_CONTENT']")
+        
+        soup_p = soup_class[0].findAll('p')
+        
+        x_content = ''
+        for x in soup_p:
+            x_content = '%s\n\n%s'%(x_content,x.text)
+        
+        content = '本周< %s >星座運勢解析\n%s ~ %s%s'%(res,weekdate_s,weekdate_e,x_content)
+        
+        #soup_span = soup_class[0].select("span[class='txt_green']")[0].text
+        
+#        f=open('output.txt','w', encoding='UTF-8')
+#        f.write(format(soup))
+#        f.close()
+
+        return content
+
+
+
+    
+    def starnumber(self,res):
+        
+        if res == '牡羊座':
+            return 0
+        if res == '金牛座':
+            return 1
+        if res == '雙子座':
+            return 2
+        if res == '巨蟹座':
+            return 3
+        if res == '獅子座':
+            return 4
+        if res == '處女座':
+            return 5
+        if res == '天秤座':
+            return 6
+        if res == '天蠍座':
+            return 7
+        if res == '射手座':
+            return 8
+        if res == '摩羯座':
+            return 9
+        if res == '水瓶座':
+            return 10
+        if res == '雙魚座':
+            return 11
     
     #巴哈姆特    
     def gamer(self,res):
