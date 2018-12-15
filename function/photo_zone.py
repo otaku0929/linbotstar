64321f5c5e992056c34b696862cf08dfd0e07e7d
Victor <32451715+otaku0929@users.noreply.github.com>
2018-12-15 08:51:20 +0800

    Add files via upload

--- function/photo_zone.py
+++ function/photo_zone.py
@@ -204,7 +204,7 @@ class photo_zone(object):
     
     def user_daily_photo(self,id,message,pictureUrl):
         
-        #try:
+        try:
             path = 'jpg/' 
             #path = '../jpg/'
             _card_template = '%scard_template.jpg'%path
@@ -293,8 +293,8 @@ class photo_zone(object):
             
             #imgur_dict = ['otaku','twstar','otakuboy']
             #imgur_dict = ['otaku','otakuboy']
-            imgur_dict = ['otakuboy']
-            #imgur_dict = ['twstar']
+            #imgur_dict = ['otakuboy']
+            imgur_dict = ['twstar']
             #imgur_dict = ['otaku']
             imgur_upload = random.choice(imgur_dict)
             
@@ -310,9 +310,9 @@ class photo_zone(object):
                 res = self.upload_imgur_otakuboy('Ub4VfUk',card_template)
                 #print('otakuboy')
                 return ('1',res)                
-        #except:
-        #    return ('0','卡片產生失敗，重新印制卡片功能開發中，請先用[查人物屬性]指令查看今天的屬性，並進行對戰')
-    
+        except:
+            #return ('0','卡片產生失敗，重新印制卡片功能開發中，請先用[查人物屬性]指令查看今天的屬性，並進行對戰')
+            return ('0','因流量超出限制，卡片產生功能調整中，請先用[查人物屬性]指令查看今天的屬性，並進行對戰')
     
     def add_words(self,id,text,fontsize, px, py, fontname,template_img, pxx=None, pyy=None):
 
